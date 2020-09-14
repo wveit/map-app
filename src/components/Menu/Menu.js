2198915fdc8d25fc5058140e5baed66e8e14cd04
Wilbert Veit <wilbert.e.veit@jpl.nasa.gov>
2020-09-14 16:06:05 -0700

    Enable collapsing the menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -35,24 +35,57 @@ function LayerComponent(props) {
     );
 }
 
-function Menu(props) {
-    const layerDataComponents = props.layerData.map(function (layer, index) {
-        return (
-            <LayerComponent
-                layer={layer}
-                key={index}
-                toggleLayerVisibility={props.toggleLayerVisibility}
-                adjustLayerOpacity={props.adjustLayerOpacity}
-            />
-        );
-    });
+class Menu extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            collapsed: false,
+        };
+        this.handleCollapseClick = this.handleCollapseClick.bind(this);
+    }
 
-    return (
-        <div id="Menu">
-            <div className="Menu__title">State of the Ocean</div>
-            {layerDataComponents}
-        </div>
-    );
+    handleCollapseClick() {
+        this.setState(function (state) {
+            return { collapsed: !state.collapsed };
+        });
+    }
+
+    render() {
+        const props = this.props;
+        const layerDataComponents = this.props.layerData.map(function (
+            layer,
+            index
+        ) {
+            return (
+                <LayerComponent
+                    layer={layer}
+                    key={index}
+                    toggleLayerVisibility={props.toggleLayerVisibility}
+                    adjustLayerOpacity={props.adjustLayerOpacity}
+                />
+            );
+        });
+
+        const collapseButtonSymbol = this.state.collapsed ? (
+            <span>&gt;</span>
+        ) : (
+            <span>&lt;</span>
+        );
+
+        return (
+            <div id="Menu" className={this.state.collapsed ? "collapsed" : ""}>
+                <div className="Menu__titlebar">
+                    <div className="Menu__title">State of the Ocean</div>
+                    <div className="Menu__controls">
+                        <button onClick={this.handleCollapseClick}>
+                            {collapseButtonSymbol}
+                        </button>
+                    </div>
+                </div>
+                <div className="Menu__content">{layerDataComponents}</div>
+            </div>
+        );
+    }
 }
 
 function mapStateToProps({ layerData }) {
