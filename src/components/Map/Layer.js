283595b1d5786cc653fb7b679fdc279b2975c889
Wilbert Veit <wilbert.e.veit@jpl.nasa.gov>
2020-09-14 03:16:41 -0700

    Change map to openlayers

--- src/components/Map/Layer.js
+++ src/components/Map/Layer.js
@@ -1,18 +1,62 @@
 import React from "react";
+import OlSource from "ol/source/WMTS";
+import OlLayer from "ol/layer/Tile";
+import OlTilegrid from "ol/tilegrid/WMTS";
 
-export default function Layer(props) {
-    if (!props.layer.visible) return null;
+class Layer extends React.Component {
+    constructor() {
+        super();
+        this.source = null;
+        this.layer = null;
+    }
 
-    const url = props.layer.url
-        .replace("{Time}", "Default")
-        .replace("{TileMatrixSet}", props.layer.tileMatrixSet.identifier)
-        .replace("{TileMatrix}", 0)
-        .replace("{TileRow}", 0)
-        .replace("{TileCol}", 0);
+    render() {
+        return null;
+    }
 
-    return (
-        <div className={`Layer layer-${props.layer.identifier}`}>
-            <img src={url} alt="" />
-        </div>
-    );
+    componentDidMount() {
+        console.log("mounted layer: " + this.props.layer.identifier);
+
+        this.source = new OlSource({
+            url: this.props.layer.url.replace("{Time}", "default"),
+            requestEncoding: "REST",
+            layer: this.props.layer.identifier,
+            format: "image/jpeg",
+            matrixSet: this.props.layer.tileMatrixSet.identifier,
+            tileGrid: new OlTilegrid({
+                origin: [-180, 90],
+                resolutions: [
+                    0.5625,
+                    0.28125,
+                    0.140625,
+                    0.0703125,
+                    0.03515625,
+                    0.017578125,
+                    0.0087890625,
+                    0.00439453125,
+                    0.002197265625,
+                ],
+                matrixIds: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                tileSize: 512,
+            }),
+        });
+
+        this.layer = new OlLayer({
+            source: this.source,
+            extent: [-180, -90, 180, 90],
+        });
+
+        this.props.map.addLayer(this.layer);
+    }
+
+    componentDidUpdate() {
+        console.log("updated layer: " + this.props.layer.identifier);
+    }
+
+    componentWillUnmount() {
+        console.log("unmounted layer: " + this.props.layer.identifier);
+        this.props.map.removeLayer(this.layer);
+    }
 }
+
+export default Layer;
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,17 +1,60 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import "ol/ol.css";
+import { Map, View } from "ol";
+import { get as getProjection } from "ol/proj";
+
 import Layer from "./Layer";
 
-function Map(props) {
-    const layerComponents = props.layers.map((layer) => (
-        <Layer layer={layer} key={layer.identifier} />
-    ));
-    return (
-        <div id="Map">
-            <div id="Map__world">{layerComponents}</div>
-        </div>
-    );
+class MyMap extends React.Component {
+    constructor(props) {
+        super(props);
+        this.map = null;
+        this.state = {
+            mapIsLoaded: false,
+        };
+    }
+
+    render() {
+        const { date } = this.props;
+        const map = this.map;
+        if (!this.state.mapIsLoaded) {
+            return null;
+        }
+        const layers = this.props.layers.map(function (layer) {
+            return (
+                <Layer
+                    map={map}
+                    layer={layer}
+                    date={date}
+                    key={layer.identifier}
+                />
+            );
+        });
+        return <>{layers}</>;
+    }
+
+    componentDidMount() {
+        this.map = window.map = new Map({
+            target: "Map",
+            layers: [],
+            view: new View({
+                projection: getProjection("EPSG:4326"),
+                center: [0, 0],
+                zoom: 0,
+                extent: [-500, -200, 500, 200],
+            }),
+        });
+        this.setState({ mapIsLoaded: true });
+        console.log("Map mounted");
+    }
+
+    componentDidUpdate() {
+        console.log("Map updated", this.props.layers);
+    }
+
+    componentWillUnmount() {}
 }
 
 function mapStateToProps(state) {
@@ -20,4 +63,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(Map);
+export default connect(mapStateToProps)(MyMap);
