b33278f96850f3dbfba70324d0e8964e59a608fc
Wilbert Veit <wilbert.e.veit@jpl.nasa.gov>
2020-09-14 03:41:39 -0700

    Added MUR SST layers

--- src/util/layerLoader.js
+++ src/util/layerLoader.js
@@ -4,6 +4,8 @@ import { addLayerData } from "../actions/layerData";
 const configuredLayers = [
     "BlueMarble_ShadedRelief",
     "GHRSST_L4_AVHRR-OI_Sea_Surface_Temperature",
+    "GHRSST_L4_MUR25_Sea_Surface_Temperature",
+    "GHRSST_L4_MUR_Sea_Surface_Temperature",
     "Aquarius_Sea_Surface_Salinity_L3_7Day_RunningMean",
     "AIRS_L2_Surface_Air_Temperature_Day",
 ];
